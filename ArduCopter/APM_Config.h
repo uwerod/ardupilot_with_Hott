abd982313c5e8ff1989c01daf9f92218032c1e2b
uwerod <u.m.r@gmx.de>
2013-09-18 15:04:11 +0200

    Update APM_Config.h

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -10,7 +10,7 @@
 //#define DMP_ENABLED           ENABLED             // use MPU6000's DMP instead of DCM for attitude estimation
 //#define SECONDARY_DMP_ENABLED ENABLED             // allows running DMP in parallel with DCM for testing purposes
 
-//#define FRAME_CONFIG QUAD_FRAME
+#define FRAME_CONFIG HEXA_FRAME
 /*
  *  options:
  *  QUAD_FRAME
@@ -26,9 +26,24 @@
 // Put your variable definitions into the UserVariables.h file (or another file name and then change the #define below).
 //#define USERHOOK_VARIABLES "UserVariables.h"
 // Put your custom code into the UserCode.pde with function names matching those listed below and ensure the appropriate #define below is uncommented below
-//#define USERHOOK_INIT userhook_init();                      // for code to be run once at startup
+#define USERHOOK_INIT userhook_init();                      // for code to be run once at startup
 //#define USERHOOK_FASTLOOP userhook_FastLoop();            // for code to be run at 100hz
-//#define USERHOOK_50HZLOOP userhook_50Hz();                  // for code to be run at 50hz
+#define USERHOOK_50HZLOOP userhook_50Hz();                  // for code to be run at 50hz
 //#define USERHOOK_MEDIUMLOOP userhook_MediumLoop();        // for code to be run at 10hz
 //#define USERHOOK_SLOWLOOP userhook_SlowLoop();            // for code to be run at 3.3hz
 //#define USERHOOK_SUPERSLOWLOOP userhook_SuperSlowLoop();  // for code to be run at 1hz
+
+//
+//	HoTT definitions
+//
+#define HOTT_TELEMETRY
+#define HOTT_TELEMETRY_SERIAL_PORT	2
+#define HOTT_SIM_GPS_SENSOR
+#define HOTT_SIM_EAM_SENSOR
+#define HOTT_SIM_VARIO_SENSOR
+//#define HOTT_SIM_GAM_SENSOR
+// #define HOTT_SIM_TEXTMODE
+//Textmode address to simulate
+// #define HOTT_SIM_TEXTMODE_ADDRESS	HOTT_TELEMETRY_GPS_SENSOR_ID
+//#define CONFIG_SONAR_SOURCE_ANALOG_PIN 0
+
